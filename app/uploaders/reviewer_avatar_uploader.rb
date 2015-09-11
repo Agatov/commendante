26cfbcb58c6a48607ef642992c3f9fe79fb29713
Kirill Agatov <abardacha@gmail.com>
2015-09-11 16:39:02 +0300

    аватарка ресайзится теперь по другому

--- app/uploaders/reviewer_avatar_uploader.rb
+++ app/uploaders/reviewer_avatar_uploader.rb
@@ -15,7 +15,7 @@ class ReviewerAvatarUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
 
   version :preview do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fill => [100, 100]
   end
 
 
