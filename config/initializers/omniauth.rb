beec240570e458f85f10db5856595f7505a17d36
Kirill Agatov <abardacha@gmail.com>
2015-09-11 14:21:04 +0300

    facebook ololo2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,12 @@ case HOSTNAME
     Rails.application.config.middleware.use OmniAuth::Builder do
       provider :vkontakte, "5065287", "c7nyUUWlnE7JYxwF47aL"
       provider :odnoklassniki, "1154112000", "D3C0988A605912684E5BF553", :public_key => "CBALFLMFEBABABABA"
-      provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5"
+      provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5", scope: 'email, public_profile', display: 'popup'
     end
   else
     Rails.application.config.middleware.use OmniAuth::Builder do
       provider :vkontakte, "5017668", "LitPB4X9zlyz9VSzOfR2"
       provider :odnoklassniki, "1154112000", "D3C0988A605912684E5BF553", :public_key => "CBALFLMFEBABABABA"
-      provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5"
+      provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5", scope: 'email, public_profile', display: 'popup'
     end
 end
