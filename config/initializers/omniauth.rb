44a66b0f762526da4d9e94ba9d24e6ce926230f8
Kirill Agatov <abardacha@gmail.com>
2015-09-11 15:17:32 +0300

    facebook ololo9

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,8 @@ case HOSTNAME
       provider :odnoklassniki, "1154112000", "D3C0988A605912684E5BF553", :public_key => "CBALFLMFEBABABABA"
       provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5", 
         scope: 'email,public_profile',
-        info_fields: 'name,email,nickname,first_name,last_name,image,urls'
+        info_fields: 'id,email,gender,link,locale,name,timezone,updated_time,verified',
+        callback_url: 'http://getreview.ru/auth/facebook/callback'
     end
   else
     Rails.application.config.middleware.use OmniAuth::Builder do
@@ -13,6 +14,7 @@ case HOSTNAME
       provider :odnoklassniki, "1154112000", "D3C0988A605912684E5BF553", :public_key => "CBALFLMFEBABABABA"
       provider :facebook, "958836017510529", "1edcce7ebd92b3f2f44c1618d9b646f5", 
         scope: 'email,public_profile',
-        info_fields: 'id,email,gender,link,locale,name,timezone,updated_time,verified'
+        info_fields: 'id,email,gender,link,locale,name,timezone,updated_time,verified',
+        callback_url: 'http://getreview.ru/auth/facebook/callback'
     end
 end
